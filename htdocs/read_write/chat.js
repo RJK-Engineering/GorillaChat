35d7d9fafd90730725feff36a439f2c312a8e5f7
RJK-Engineering <rjklinkhamer@gmail.com>
2018-01-18 14:12:25 +0100

    Version 3

--- htdocs/read_write/chat.js
+++ htdocs/read_write/chat.js
@@ -1,10 +1,19 @@
 // MooTools domready function
 window.addEvent('domready', function() {
     AddEvents();
+    ListMessages();
     SetUsername('Anonymous');
-    // LoadMessages();
-    GetNextMessage();
+
+    // schedule poll
+    setTimeout(GetNextMessage, pollingInterval);
 });
+
+/* CONFIGURATION */
+// var apiUrl = 'http://www.codegorilla.nl/read_write/api.php';
+// var apiUrl = 'http://10.11.13.231/read_write/api.php';
+var apiUrl = 'http://localhost/read_write/api.php';
+var pollingInterval = 2000; // delay (in ms) before polling for next message
+var defaultListSize = 10;
 
 function AddEvents() {
     $$('#mykey').addEvents({
@@ -15,7 +24,11 @@ function AddEvents() {
     $$('#value').addEvents({
         keyup: function(e) {
             if (e.code == 13) {
-                SendMessage(this.value);
+                if (this.value[0] == '.') {
+                    ExecuteCommand(this.value);
+                } else {
+                    SendMessage(this.value);
+                }
             }
         }
     });
@@ -24,13 +37,42 @@ function AddEvents() {
 var userName;
 var nextId;
 
+function ExecuteCommand(msg) {
+    $$('#value').set('value', '');
+    var myRegexp = /^\.(\w+)(\s+.*)?/;
+    var match = myRegexp.exec(msg);
+    if (match) {
+        var cmd = match[1];
+        var args = match[2];
+        if (cmd == 'list') {
+            ListMessages(args);
+        } else if (cmd == 'name') {
+            SetUsername(args);
+        } else if (cmd == 'reset') {
+            $$('#messages').set('text', '');
+        } else {
+            DisplayErrorMessage("Invalid command: " + cmd);
+        }
+    } else {
+        DisplayErrorMessage("Error processing command");
+    }
+}
+
 function SetUsername(name) {
+    if (! name) {
+        DisplayErrorMessage("No name provided");
+        return;
+    }
     userName = name;
     $$('#mykey').set('value', userName);
     SendMessage('has entered the chat');
 }
 
 function SendMessage(message) {
+    if (! message) {
+        DisplayErrorMessage("You didn't type anything!");
+        return;
+    }
     writeRequest.get({
         action: 'write',
         mykey: userName,
@@ -39,45 +81,85 @@ function SendMessage(message) {
     DisplayMessage(userName, message);
 }
 
-function DisplayMessage(userName, message) {
-    $$('#messages').appendHTML('<b>' + userName + '</b> ' + message + '<br>');
-    // $$('#messages').set('text', userName + ' ' + message);
-    // alert (userName + ' ' + message);
-}
-
 function GetNextMessage() {
     readRequest.get({
         action: 'read',
         id: nextId
     });
-
-    setTimeout(GetNextMessage, 2000);
+    // schedule next poll
+    setTimeout(GetNextMessage, pollingInterval);
 }
 
-var url = 'http://localhost/read_write/api.php';
+function ListMessages(limit) {
+    if (! limit) { limit = defaultListSize; }
+    listRequest.get({
+        action: 'list',
+        limit: limit
+    });
+}
+
+function DisplayMessage(userName, message) {
+    Display(
+        '<span class="userName">' + userName + '</span> '
+        + message + '<br>'
+    );
+}
+
+function DisplayErrorMessage(message) {
+    Display('<span class="error">' + message + '</span><br>');
+}
+
+function Display(html) {
+    var div = $$('#messages');
+    div.appendHTML(html);
+    // scroll to end of div
+    div.scrollTo(0, div.getScrollSize()[0].y);
+}
 
 var writeRequest = new Request.JSON({
-    url: url,
+    url: apiUrl,
     method: 'get',
-    // onRequest: function() {
-    // },
-    onSuccess: function(json, text) {
-        // alert (json);
-        nextId = json +  1;
+    onRequest: function() {
         $$('#value').set('value', '');
+        $$('#sending').set('text', 'sending...');
     },
-    // onFailure: function() {
-    //     alert('fail');
-    // }
+    onSuccess: function(json, text) {
+        nextId = parseInt(json) +  1;
+        $$('#sending').set('text', '');
+    },
+    onFailure: function() {
+        DisplayErrorMessage("Error sending message, please try again");
+        $$('#sending').set('text', '');
+    }
 });
 
 var readRequest = new Request.JSON({
-    url: url,
+    url: apiUrl,
     method: 'get',
     onSuccess: function(json, text) {
         if (json) {
             DisplayMessage(json.mykey, json.value);
-            nextId = json.id + 1;
+            nextId = parseInt(json.id) + 1;
         }
+    }
+});
+
+var listRequest = new Request.JSON({
+    url: apiUrl,
+    method: 'get',
+    onRequest: function() {
+        $$('#sending').set('text', 'sending...');
+    },
+    onSuccess: function(json, text) {
+        $$('#sending').set('text', '');
+        if (json) {
+            json.each(function (msg) {
+                DisplayMessage(msg.mykey, msg.value);
+            });
+        }
+    },
+    onFailure: function() {
+        DisplayErrorMessage("Error sending command, please try again");
+        $$('#sending').set('text', '');
     }
 });
